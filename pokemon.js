de25874220066d2287af2c05c99c5134e1fa7c0c
Jason <jrosasrjf@gmail.com>
2021-03-24 19:47:36 -0500

    search history now populates with recent searches. Working on adding to local storage

--- pokemon.js
+++ pokemon.js
@@ -1,25 +1,47 @@
 var searchSubmitButtonE1 = $("#searchSubmitButton");
 var recentCitySearch1E1 = $("#recentCitySearch1"); // The city blocks displayed in search history 
 
-
-
+var searchHistoryArray= [];
+var searchHistoryArrayIndex = 0;
 
 
 searchSubmitButtonE1.on("click", function(event){
+    // triggers off of button click
+    
     event.preventDefault();
-
-
     var citySearchBoxText = document.getElementById("searchInput").value; // Text we want stored. From the search bar
     recentCitySearch1E1.text(citySearchBoxText); // Displays to screen using what was typed into search bar
 
+    localStorage.setItem("searchInputStorage", citySearchBoxText); // like declaring a new variable. 
 
-    localStorage.setItem("searchInputStorage", citySearchBoxText); //like declaring a new variable. The set Item always sa
- // localStorage.setItem(what you're storing to, what you are actually storing)
 
+    searchHistoryPopulate(citySearchBoxText); // passes on the city name
 })
 
 
+function searchHistoryPopulate(cityName) {
+    // triggered by search button press 
+    searchHistoryArrayIndex += 1;
+    console.log("after incrament " + searchHistoryArrayIndex + " | City name is " + cityName);
+
+
+    searchHistoryListE1 = document.getElementById("searchHistoryList");
+    var newHeading = document.createElement("li");
+    newHeading.innerText = cityName;
+    searchHistoryListE1.appendChild(newHeading);
+
+
+}
+
+
+
+
+
+/*      --          local storage          --      */
+
 $("#recentCitySearch1").text(localStorage.getItem("searchInputStorage"));
+
+
 
 
 
@@ -28,11 +50,13 @@ $("#recentCitySearch1").text(localStorage.getItem("searchInputStorage"));
 
 . searchSubmitButtonE1 | References search button in HTML
 . recentCitySearch1E1  | References text for HTML display 
-. citySearchBoxText    | References what was typed into the search bar
+. 
 .
-. citySearchBoxSave    | Changes recentCitySearch1E1 to whatever was typed in for citySearchBoxText 
+.
 .
 
+git push like this below -- 
+git push origin localStorage
 
 
 */
