bb3ca980201d1a15cc195f9b8498ef1da76bf4aa
Jason <jrosasrjf@gmail.com>
2021-03-24 20:54:40 -0500

    created elements contain city names along with IDs which reference the array index. Scoping variable issues prevent local storage access. I think I know a work around for that

--- pokemon.js
+++ pokemon.js
@@ -12,35 +12,54 @@ searchSubmitButtonE1.on("click", function(event){
     var citySearchBoxText = document.getElementById("searchInput").value; // Text we want stored. From the search bar
     recentCitySearch1E1.text(citySearchBoxText); // Displays to screen using what was typed into search bar
 
-    localStorage.setItem("searchInputStorage", citySearchBoxText); // like declaring a new variable. 
-
-
     searchHistoryPopulate(citySearchBoxText); // passes on the city name
 })
 
 
 function searchHistoryPopulate(cityName) {
     // triggered by search button press 
-    searchHistoryArrayIndex += 1;
-    console.log("after incrament " + searchHistoryArrayIndex + " | City name is " + cityName);
+    
+    searchHistoryArrayIndex += 1; // incriments index per push 
+    searchHistoryArray.push(cityName); // adds last city name to this array
 
-
-    searchHistoryListE1 = document.getElementById("searchHistoryList");
+    
+    // displays to screen 
+    var searchHistoryListE1 = document.getElementById("searchHistoryList");
     var newHeading = document.createElement("li");
+    
+    var idTag = "searchInput" + searchHistoryArrayIndex;
+        //var idTag = "searchInput" + searchHistoryArrayIndex;
+    
+    newHeading.setAttribute("id", idTag);
     newHeading.innerText = cityName;
     searchHistoryListE1.appendChild(newHeading);
 
+    // sets to local storage
+    var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
+        // var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
+    localStorage.setItem(storageLocal, cityName); // like declaring a new variable. 
+    localStorage.setItem("searchInputStorage", cityName); // for the first text under search history 1
 
+    // debugging 
+    console.log(searchHistoryArray);
+    console.log("-- idTag & index " + idTag + " | getElement result " + newHeading.getAttribute("id"));
+    console.log("the storageLocal " + storageLocal);
 }
 
 
+$("#recentCitySearch1").text(localStorage.getItem("searchInputStorage"));
 
 
 
 /*      --          local storage          --      */
 
-$("#recentCitySearch1").text(localStorage.getItem("searchInputStorage"));
 
+// original 
+//localStorage.setItem("searchInputStorage", cityName); // like declaring a new variable.
+//$("#recentCitySearch1").text(localStorage.getItem("searchInputStorage"));
+
+//$(idTag).text(localStorage.getItem(storageLocal));
+//$(HTMLreference ID).text(localStorage.getItem(where it was stored))
 
 
 
