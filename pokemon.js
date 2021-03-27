27b184ee95d266d7b2386964e98a6226e33cda9d
Jason <jrosasrjf@gmail.com>
2021-03-27 12:31:07 -0500

    array now appears on screen as a search history. array will take undefined elements into local storage, which displays undefined. Looking into locally storing all elements into an array, not just for the display

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,14 @@
 var searchSubmitButtonE1 = $("#searchSubmitButton");
-var recentCitySearch1E1 = $("#recentCitySearch1"); // The city blocks displayed in search history 
 
-var searchHistoryArray= [];
+var recentCitySearch1E1 = $("#recentCitySearch1"); // The city blocks displayed in search history 
+var recentCitySearch2E1 = $("#recentCitySearch2");
+var recentCitySearch3E1 = $("#recentCitySearch3");
+var recentCitySearch4E1 = $("#recentCitySearch4");
+var recentCitySearch5E1 = $("#recentCitySearch5");
+var recentCitySearch6E1 = $("#recentCitySearch6");
+
+
+var searchHistoryArray = [];
 var searchHistoryArrayIndex = 0;
 var searchButtonPresses = 0;
 
@@ -39,35 +46,24 @@ function searchHistoryPopulate(cityName) {
     }
     
 
+    // Displays to screen under Recent Search History
+    recentCitySearch1E1.text(searchHistoryArray[0]); // Displays to screen using what was typed into search bar. Appears in the search box with the green dashed borders
+    recentCitySearch2E1.text(searchHistoryArray[1]);
+    recentCitySearch3E1.text(searchHistoryArray[2]);
+    recentCitySearch4E1.text(searchHistoryArray[3]);
+    recentCitySearch5E1.text(searchHistoryArray[4]);
+    recentCitySearch6E1.text(searchHistoryArray[5]);
 
-    // creates and determines what elements will be displayed on screen.
-    
-    
-    
-    // where you left off yesterday                 ---             - --    ----    ----
-
-
-
-    newHeading.setAttribute("id", idTag);
-    newHeading.innerText = searchHistoryArray; // access the array and populates the required city
-    searchHistoryListE1.appendChild(newHeading);
-    
-
- 
-    
      
     // sets to local storage
-    var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
-        // var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
-    localStorage.setItem(storageLocal, cityName); // like declaring a new variable. 
-    localStorage.setItem("searchInputStorage", cityName); // for the first text under search history 1
-
-
-
-
-    // Once the dust settles
-    recentCitySearch1E1.text(cityName); // Displays to screen using what was typed into search bar. Appears in the search box with the green dashed borders
+    localStorage.setItem("searchInputStorage1", searchHistoryArray[0]); // for the first text under search history 1
+    localStorage.setItem("searchInputStorage2", searchHistoryArray[1]);
+    localStorage.setItem("searchInputStorage3", searchHistoryArray[2]);
+    localStorage.setItem("searchInputStorage4", searchHistoryArray[3]);
+    localStorage.setItem("searchInputStorage5", searchHistoryArray[4]);
+    localStorage.setItem("searchInputStorage6", searchHistoryArray[5]);
     
+
     if (searchHistoryArrayLength < desiredArrayLength) {
         // triggers only if the current is less than a certain amount. 
 
@@ -78,13 +74,17 @@ function searchHistoryPopulate(cityName) {
     // debugging 
     console.log("\n");
     console.log(searchHistoryArray);
-    console.log("New Heading ID " + newHeading.getAttribute("id") + " | local storage name " + storageLocal + " | Current Array Index " + searchHistoryArrayIndex + " | array Length " + searchHistoryArrayLength);
+    console.log("New Heading ID " + newHeading.getAttribute("id") + " | Current Array Index " + searchHistoryArrayIndex + " | array Length " + searchHistoryArrayLength);
     
 }
 
 
-$("#recentCitySearch1").text(localStorage.getItem("searchInputStorage"));
-
+$("#recentCitySearch1").text(localStorage.getItem("searchInputStorage1"));
+$("#recentCitySearch2").text(localStorage.getItem("searchInputStorage2"));
+$("#recentCitySearch3").text(localStorage.getItem("searchInputStorage3"));
+$("#recentCitySearch4").text(localStorage.getItem("searchInputStorage4"));
+$("#recentCitySearch5").text(localStorage.getItem("searchInputStorage5"));
+$("#recentCitySearch6").text(localStorage.getItem("searchInputStorage6"));
 
 
 /*      --          local storage          --      */
@@ -137,6 +137,28 @@ for(var i = 0; i < searchHistoryArrayIndex; i++) {
         searchHistoryListE1.appendChild(newHeading);
     }
 
+
+
+
+
+    // sets to local storage
+    var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
+        // var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
+    localStorage.setItem(storageLocal, cityName); // like declaring a new variable. 
+    localStorage.setItem("searchInputStorage", cityName); // for the first text under search history 1
+
+
+
+
+    // Once the dust settles
+    recentCitySearch1E1.text(cityName); // Displays to screen using what was typed into search bar. Appears in the search box with the green dashed borders
+
+
+    // sets to local storage
+    var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
+        // var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
+    localStorage.setItem(storageLocal, cityName); // like declaring a new variable. 
+    localStorage.setItem("searchInputStorage", cityName); // for the first text under search history 1
 
 
 
