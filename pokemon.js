72fba9f4dd5c0cde3f5abb8b072a212648e31ad7
Jason <jrosasrjf@gmail.com>
2021-03-27 10:43:13 -0500

    cleaned up code. Added array functionality. Unshifts search elements into array. Displays entire array per search history. Working on displaying individual elements

--- pokemon.js
+++ pokemon.js
@@ -3,47 +3,83 @@ var recentCitySearch1E1 = $("#recentCitySearch1"); // The city blocks displayed 
 
 var searchHistoryArray= [];
 var searchHistoryArrayIndex = 0;
+var searchButtonPresses = 0;
 
 
 searchSubmitButtonE1.on("click", function(event){
     // triggers off of button click
-    
     event.preventDefault();
-    var citySearchBoxText = document.getElementById("searchInput").value; // Text we want stored. From the search bar
-    recentCitySearch1E1.text(citySearchBoxText); // Displays to screen using what was typed into search bar
 
-    searchHistoryPopulate(citySearchBoxText); // passes on the city name
+    var citySearchBoxText = document.getElementById("searchInput").value; // Text we want stored. From the search bar
+    searchHistoryPopulate(citySearchBoxText); // passes on the city name, which then is used to populate the searchHistoryArray
 })
 
 
 function searchHistoryPopulate(cityName) {
     // triggered by search button press 
     
-    searchHistoryArrayIndex += 1; // incriments index per push 
-    searchHistoryArray.push(cityName); // adds last city name to this array
 
+    // Create elements which display to the screen as bullet points
+    var searchHistoryListE1 = document.getElementById("searchHistoryList"); // id for the unorganized listed in html. The containder for the  
+    var newHeading = document.createElement("li"); // creates the bullet point which will contain a search input. 
+    var idTag = "searchInput" + searchHistoryArrayIndex; // creates the ID tag used in the display. ID incrments per iteration of this function
+
+
+    // Updates the searchHistoryArray values
+    searchHistoryArray.unshift(cityName); // adds last city name to beginning of the array. 
+    searchHistoryArrayLength = searchHistoryArray.length; 
+    desiredArrayLength = 7;
+
+    if (searchHistoryArrayLength >= desiredArrayLength) {
+        // this happens when there are too many searches.
+
+        searchHistoryArray.pop(); // removes the oldest search once we reach our desired limit. Keeps the array max length equal to desiredArrayLength.
+        searchHistoryArrayIndex = desiredArrayLength - 1; // assures the index does not exceed the maximum desired length. Important for referencing.   
+        // index will always be one below the current array length
+    }
     
-    // displays to screen 
-    var searchHistoryListE1 = document.getElementById("searchHistoryList");
-    var newHeading = document.createElement("li");
+
+
+    // creates and determines what elements will be displayed on screen.
     
-    var idTag = "searchInput" + searchHistoryArrayIndex;
-        //var idTag = "searchInput" + searchHistoryArrayIndex;
     
+    
+    // where you left off yesterday                 ---             - --    ----    ----
+
+
+
     newHeading.setAttribute("id", idTag);
-    newHeading.innerText = cityName;
+    newHeading.innerText = searchHistoryArray; // access the array and populates the required city
     searchHistoryListE1.appendChild(newHeading);
+    
 
+ 
+    
+     
     // sets to local storage
     var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
         // var storageLocal = "searchInputStorage" + searchHistoryArrayIndex;
     localStorage.setItem(storageLocal, cityName); // like declaring a new variable. 
     localStorage.setItem("searchInputStorage", cityName); // for the first text under search history 1
 
+
+
+
+    // Once the dust settles
+    recentCitySearch1E1.text(cityName); // Displays to screen using what was typed into search bar. Appears in the search box with the green dashed borders
+    
+    if (searchHistoryArrayLength < desiredArrayLength) {
+        // triggers only if the current is less than a certain amount. 
+
+        searchHistoryArrayIndex += 1; // incriments index per push.  
+    }
+  
+    
     // debugging 
+    console.log("\n");
     console.log(searchHistoryArray);
-    console.log("-- idTag & index " + idTag + " | getElement result " + newHeading.getAttribute("id"));
-    console.log("the storageLocal " + storageLocal);
+    console.log("New Heading ID " + newHeading.getAttribute("id") + " | local storage name " + storageLocal + " | Current Array Index " + searchHistoryArrayIndex + " | array Length " + searchHistoryArrayLength);
+    
 }
 
 
@@ -82,3 +118,26 @@ git push origin localStorage
 
 
 
+/* this was working before
+
+
+    // creates and determines what elements will be displayed on screen. 
+
+    newHeading.setAttribute("id", idTag);
+    newHeading.innerText = cityName;
+    searchHistoryListE1.appendChild(newHeading);
+
+
+
+for(var i = 0; i < searchHistoryArrayIndex; i++) {
+        
+        
+        newHeading.setAttribute("id", idTag);
+        newHeading.innerText = searchHistoryArray[i]; // access the array and populates the required city
+        searchHistoryListE1.appendChild(newHeading);
+    }
+
+
+
+
+*/
